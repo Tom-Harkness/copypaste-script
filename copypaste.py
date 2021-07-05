6e491fea29572a7addda63b5861b18f4ead30239
Tom-Harkness <tom-harkness@hotmail.com>
2021-07-06 05:14:21 +1000

    Finished copypaste script to current requirements

--- copypaste.py
+++ copypaste.py
@@ -4,13 +4,22 @@
 """Script to replicate Ctrl-V paste by the following steps:
     - Use pynput to listen for Ctrl-Shift-v or ESC hotkeys
     - Once hotkey is detected, access clipboard contents with pywin32's win32clipboard module
-    - Write clipboard contents with keyboard module"""
+    - Write clipboard contents with keyboard module
+Optional:
+ - builds exe with pyinstaller with command: pyinstaller copypaste.py --hidden-import "pynput.keyboard._win32" -F
+ - copypaste.exe found in ./dist folder"""
 
 from datetime import datetime
 import keyboard
 import pynput
 import win32clipboard
 import os
+
+
+def empty_clipboard():
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.CloseClipboard()
 
 
 def on_activate_paste():
@@ -25,35 +34,34 @@ def on_activate_paste():
     try:
         assert(clipboard_text is not None)
         print(f"{datetime.now()} - Pasting: {clipboard_text}")
-        keyboard.release('ctrl+shift+z')
+        keyboard.release('ctrl+shift+v')
         keyboard.write(clipboard_text)
     except AssertionError:
         print(f"{datetime.now()} - Nothing on clipboard.")
 
 
 def on_activate_esc():
-    print(f"Esc pressed... Exiting script at {datetime.now()}")
+    print(f"\nEsc pressed... Exiting script at {datetime.now()}\n")
     exit()
 
 
 def for_canonical(f):
-    return lambda k: f(listener.canonical(k))
+    return lambda k: f(listener_paste.canonical(k))
 
 
 if __name__ == "__main__":
     os.system('cls')
     print("\n-- Copy/Paste script running --")
     print(f"-- Started at {datetime.now()}-- ")
-    win32clipboard.OpenClipboard()
-    win32clipboard.EmptyClipboard()
-    win32clipboard.CloseClipboard()
+    empty_clipboard()
     print("-- Clipboard has been cleared --\n")
     print("Press Ctrl-c to copy highlighted text")
-    print("Press Ctrl-Shift-v to paste current text using PyAutoGUI")
+    print("Press Ctrl-Shift-v to paste copied text")
     print("Exit script by pressing ESC or closing this console\n")
+    print("\n\tNOTE: Using Ctrl-Shift-v in a window that *does* allow pasting will cause the text to enter twice.")
+    print("\tThis script does not disable normal Ctrl-V pasting so use this when appropriate.\n")
 
-    controller = pynput.keyboard.Controller()
-
+    # Define hotkeys
     hotkey_paste = pynput.keyboard.HotKey(
         pynput.keyboard.HotKey.parse('<ctrl>+<shift>+v'),
         on_activate_paste)
@@ -63,11 +71,11 @@ if __name__ == "__main__":
         on_activate_esc)
 
     # Collect hotkey events
-    listener = pynput.keyboard.Listener(
+    listener_paste = pynput.keyboard.Listener(
             on_press=for_canonical(hotkey_paste.press),
             on_release=for_canonical(hotkey_paste.release),
             suppress=False)
-    listener.start()
+    listener_paste.start()
 
     listener_esc = pynput.keyboard.Listener(
             on_press=for_canonical(hotkey_esc.press),
